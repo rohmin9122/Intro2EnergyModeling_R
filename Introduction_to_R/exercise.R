f382adf1d3d4aff9854a0aaf5804d1817f08d211
minyoung <rohmin9122@gmail.com>
2020-09-16 14:13:35 +0900

    exercise

---
+++ Introduction_to_R/exercise.R
@@ -0,0 +1,73 @@
+require("readxl")
+require("writexl")
+
+# Set working directory
+setwd("D:/Lecture/Introduction_to_energy_modeling/R/Introduction_to_R")
+
+
+
+# constant variable
+START_YEAR <- 2000
+END_YEAR <- 2015
+CONV_KGOE_TO_TOE <- 1/1000 # 1kg of oil equivalen= 1/1000 Ton of oil equivalent
+
+
+
+# importing data of xlsx file without NA values
+getXlsxFile <- function(fileName, sheetNum, skipRow){
+  
+  filePath <- paste0("./Data/", fileName, ".xlsx")
+  
+  dat <- read_xlsx(path = filePath,
+                   sheet = sheetNum,
+                   skip = skipRow)
+  
+  # change NA to 0
+  dat[is.na(dat)] <- 0
+  
+  return(dat)
+}
+
+
+
+# load data files
+pcEn <- getXlsxFile("Energy_Use", 1, 5) # energy use per capita (unit: kgoe per capita)
+pop <- getXlsxFile("Population", 1, 5) # population (unit: one person)
+
+
+# get country code from pcEnergy data
+cntCodes <- unique(pcEn$`Country Code`)
+
+
+# create a new data frame which con
+totalEn <- data.frame(matrix(0, ncol=END_YEAR-START_YEAR+2, nrow=length(cntCodes)))
+colnames(totalEn) <- c("country.code", START_YEAR:END_YEAR)
+totalEn$country.code <- cntCodes # initialize country code
+
+
+# compute total energy
+for(code in cntCodes){
+  for (year in START_YEAR:END_YEAR) {
+    
+    col_year <- as.character(year)
+    
+    pcEn_year <- pcEn[pcEn$`Country Code`== code, col_year]
+    pop_year <- pop[pop$`Country Code`== code, col_year]
+    
+    
+    # total energy = energy per capita * population
+    # convert unit from kgoe to TOE 
+    totalEn_year <- pcEn_year * pop_year * CONV_KGOE_TO_TOE
+    
+    
+    # store the total energy into new data frame
+    totalEn[totalEn$country.code == code, col_year] <- totalEn_year
+  }
+}
+
+
+# save data as xlsx file
+write_xlsx(totalEn, path = "./Output/total_energy.xlsx")
+
+
+
